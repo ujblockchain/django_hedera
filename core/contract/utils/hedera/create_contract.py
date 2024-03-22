b85976821a8c1a1425fb8e088347a66d82993324
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-22 04:12:15 +0200

    add contract deploy utils

---
+++ core/contract/utils/hedera/create_contract.py
@@ -0,0 +1,47 @@
+import json
+
+from contract.models import DeployedContract
+from django.conf import settings
+from hedera import ContractCreateTransaction, FileCreateTransaction, Hbar
+
+from .get_client import OPERATOR_KEY, client
+
+
+# set deploy  function
+def deploy_contract():
+    # set max transaction fee
+    client.setMaxTransactionFee(Hbar(100))
+    # set max query payment fee in hbar
+    client.setMaxQueryPayment(Hbar(10))
+
+    # open and read byte code file
+    base_dir = settings.BASE_DIR
+    byte_code_file = open(f'{base_dir}/contract/utils/solidity/byte_code.json')
+    byte_code = json.load(byte_code_file)
+    byte_code_file.close()
+
+    # encode byte code
+    byte_code = byte_code['object'].encode()
+
+    # init file transaction
+    file_transaction = FileCreateTransaction()
+
+    # create file transaction for byte code
+    file_transaction_response = (file_transaction.setKeys(OPERATOR_KEY).setContents(byte_code).execute(client))
+    # transaction id
+    fileId = file_transaction_response.getReceipt(client).fileId
+    # init contract transaction
+    blockchain_transaction = ContractCreateTransaction()
+    # create hedera transaction
+    blockchain_transaction_response = (
+        blockchain_transaction.setGas(500_000).setBytecodeFileId(fileId).execute(client)
+    )
+    # get contract Id
+    contractId = blockchain_transaction_response.getReceipt(client).contractId
+
+    # save deployed contract id
+    new_contract = DeployedContract(contract_id=contractId.toString())
+    new_contract.save()
+
+    # refresh record from db
+    new_contract.refresh_from_db()

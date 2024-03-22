25066df53c20f80b8e4bc37f84b0c659e6614430
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-22 04:47:28 +0200

    add contact model

--- core/contacts/models.py
+++ core/contacts/models.py
@@ -1,3 +1,25 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+
+class Contact(models.Model):
+    message_id = ShortUUIDField(
+        length=15,
+        max_length=20,
+        alphabet='abc1249856acbw',
+        primary_key=True,
+    )
+    name = models.CharField(max_length=100, null=False, blank=False, help_text="user's name")
+    subject = models.CharField(max_length=100, null=False, blank=False, help_text='message subject')
+    reference = models.CharField(max_length=100, null=False, blank=False, help_text='message reference')
+    message = models.CharField(max_length=500, null=False, blank=False, help_text='message')
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(default=timezone.now)
+    publish = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.contract_id
+
+    class Meta:
+        ordering = 'date_created'

5dbb014a8e2cf359b78dd2a30e219ccefeca6bcd
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-22 04:47:56 +0200

    register contact model in admin

--- core/contacts/admin.py
+++ core/contacts/admin.py
@@ -1,3 +1,56 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        'message_id',
+        'name',
+        'subject',
+        'reference',
+        'message',
+        'last_updated',
+        'publish',
+    ]
+    list_display_links = [
+        'message_id',
+        'name',
+        'subject',
+        'reference',
+        'message',
+        'last_updated',
+        'publish',
+    ]
+    list_filter = ['publish']
+    readonly_fields = ['message_id']
+    search_fields = [
+        'message_id',
+        'name',
+        'subject',
+        'reference',
+        'message',
+        'last_updated',
+        'publish',
+    ]
+    list_per_page = 50
+    view_on_site = True
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fields = [
+        'message_id',
+        'name',
+        'subject',
+        'reference',
+        'message',
+        'last_updated',
+        'publish',
+    ]
+
+
+# register model in admin
+admin.site.register(Contact, ContactAdmin)

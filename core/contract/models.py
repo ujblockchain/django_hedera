d4328d9f4294e4d606ee55cee4a379ce35c79156
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-22 04:25:39 +0200

    add contract model

--- core/contract/models.py
+++ core/contract/models.py
@@ -1,3 +1,15 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class DeployedContract(models.Model):
+    contract_id = models.CharField(max_length=500, help_text='deployed contract id')
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.contract_id
+
+    class Meta:
+        verbose_name = 'Deployed Contract'
+        verbose_name_plural = 'Deployed Contract'

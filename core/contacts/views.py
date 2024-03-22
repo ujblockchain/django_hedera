68d10dab36d47f73508b53416649ade59862b461
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-22 08:13:20 +0200

    contact views

--- core/contacts/views.py
+++ core/contacts/views.py
@@ -1,3 +1,26 @@
-# from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from core.contacts.forms import ContactForm
+
+from .models import Contact
+
+
+class ContactHome(SuccessMessageMixin, CreateView):
+    form_class = ContactForm
+    model = Contact
+    success_url = 'details'
+    success_message = 'message id:%(message_id)s sent successfully'
+    template_name = 'index.html'
+
+    def get_success_url(self):
+        return reverse('details', kwargs={'pk': self.object.message_id})
+
+
+class ContactDetails(DetailView):
+    model = Contact
+    queryset = Contact.objects.filter(publish=True)
+    template_name = 'details.html'
+    context_object_name = 'contact'

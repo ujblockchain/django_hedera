2a1d5f5e27cc596561eef13b48e9bf16f06734e7
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-24 15:00:12 +0200

    reduce contract size

--- core/contract/utils/solidity/contact.sol
+++ core/contract/utils/solidity/contact.sol
@@ -1,32 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract GreenFarm {
-    address public immutable owner;
+contract MessageSender {
+    address immutable owner;
     
-    struct Contact {
-        string messageID;
-        string name;
-        string subject;
-        string ref;
-        string message;
-    }
-    
-    mapping(string => Contact) public contact;
+    mapping(string => string) public contactForm;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function setRecord(string memory messageID, string memory name_, string memory subject_, string memory ref_, string memory message_) public {
+    function setRecord(string memory messageID, string memory data_) public {
         require(msg.sender == owner, "Only the owner can update records");
-        contact[messageID].name = name_;
-        contact[messageID].subject = subject_;
-        contact[messageID].ref = ref_;
-        contact[messageID].message = message_;
+        contactForm[messageID] = data_; // data_ contains concatenation of name, subject, ref, and message
     }
 
-    function getRecord(string memory messageID) public view returns (Contact memory) {
-        return contact[messageID];
+    function getRecord(string memory messageID) public view returns (string memory) {
+        return contactForm[messageID];
     }
 }

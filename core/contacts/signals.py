78ec811e738f5a5d4c144ff8844b9a898af0acf2
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-24 18:05:21 +0200

    add user send email function

--- core/contacts/signals.py
+++ core/contacts/signals.py
@@ -1,5 +1,6 @@
 import json
 
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -12,7 +13,17 @@ from .models import Contact
 def contract_deploy_store(sender, instance, created, **kwargs):
     # ensure instance has been created
     if created:
-        # deploy contract and store record in hedera, this is get record if it exist
+        #
         message_stream = json.dumps([instance.name, instance.subject, instance.ref, instance.message])
+        # deploy contract and store record in hedera, this is get record if it exist
         receipt = record_receipt(instance.message_id, message_stream)
+        # store the transaction id in db
         sender.objects.update(transaction_id=receipt.transactionId)
+        # send email
+        send_mail(
+            subject='New Blockchain transaction',
+            message='A new message record has been stored on hedera testnet',
+            from_email='test@example.com',
+            recipient_list=['user@example.com'],
+            fail_silently=False,
+        )

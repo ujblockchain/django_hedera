ab20aca8860d1e17e858c51eb5e8e6059e0640fc
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-27 06:45:52 +0200

    update hedera get record function

--- core/contract/utils/hedera/get_record.py
+++ core/contract/utils/hedera/get_record.py
@@ -1,11 +1,14 @@
-from hedera import ContractCallQuery, ContractFunctionParameters, Hbar
+import json
+
+from hedera import ContractCallQuery, ContractFunctionParameters, ContractId, Hbar
 
 from core.contract.models import DeployedContract
 
 from .get_client import client
 
 # get contract id
-contractId = DeployedContract.objects.all()[0:1]
+contractId_string = DeployedContract.objects.all()[0:1][0].contract_id
+contractId = ContractId.fromString(contractId_string)
 
 
 def get_blockchain_record(message_id):
@@ -14,7 +17,7 @@ def get_blockchain_record(message_id):
     result = (
         ContractCallQuery().setGas(50000).setContractId(contractId).setFunction(
             'getRecord',
-            ContractFunctionParameters().get_record(message_id)
+            ContractFunctionParameters().addString(message_id)
         ).setQueryPayment(Hbar(1)).execute(client)
     )
 
@@ -23,5 +26,10 @@ def get_blockchain_record(message_id):
         exit('error calling contract: ', result.errorMessage)
     # get record
     message = result.getString(0)
+
+    # convert to list
+    # list is in [name, subject, reference, message]
+    message_list = json.loads(message)
+
     # return record
-    return {'message': message}
+    return message_list

623f1d31a16be4df9ee722333c64c894f342c2ba
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-27 06:39:38 +0200

    update detail page url path

--- core/contacts/urls.py
+++ core/contacts/urls.py
@@ -4,5 +4,5 @@ from .views import ContactDetails, ContactHome
 
 urlpatterns = [
     path('', ContactHome.as_view(), name='home'),
-    path('<int:pk>/details/', ContactDetails.as_view(), name='details'),
+    path('<str:pk>/details/', ContactDetails.as_view(), name='details'),
 ]

f5cf9009289f255ba15617f9edd8740cd48bf176
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-27 06:40:30 +0200

    switch from detail view to view

--- core/contacts/views.py
+++ core/contacts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic.detail import DetailView
+from django.views import View
 from django.views.generic.edit import CreateView
 
 from core.contacts.forms import ContactForm
+from core.contract.utils.hedera.get_record import get_blockchain_record
 
 from .models import Contact
 
@@ -12,15 +14,25 @@ class ContactHome(SuccessMessageMixin, CreateView):
     form_class = ContactForm
     model = Contact
     success_url = 'details'
-    success_message = 'message id:%(message_id)s sent successfully'
+    success_message = 'message sent successfully'
     template_name = 'index.html'
 
     def get_success_url(self):
-        return reverse('details', kwargs={'pk': self.object.message_id})
+        return reverse('details', args=[self.object.message_id])
 
 
-class ContactDetails(DetailView):
-    model = Contact
-    queryset = Contact.objects.filter(publish=True)
-    template_name = 'details.html'
-    context_object_name = 'contact'
+class ContactDetails(View):
+
+    def get(self, *args, **kwargs):
+        get_message_id = self.kwargs.get('pk')
+        template_name = 'details.html'
+
+        # get record from hedera blockchain
+        context = {
+            'contract':
+                get_blockchain_record(get_message_id),
+            'transaction_id':
+                Contact.objects.filter(message_id=get_message_id).values_list('transaction_id', flat=True)
+        }
+
+        return render(self.request, template_name, context)
